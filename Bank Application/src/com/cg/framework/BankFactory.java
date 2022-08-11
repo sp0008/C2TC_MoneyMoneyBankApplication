3c048cee31105f27cff21982df7de56b2ada87c7
Sai Priya <91816433+sp0008@users.noreply.github.com>
2022-08-11 16:25:31 +0530

    Update BankFactory.java

--- Bank Application/src/com/cg/framework/BankFactory.java
+++ Bank Application/src/com/cg/framework/BankFactory.java
@@ -3,7 +3,7 @@ package com.cg.framework;
 public abstract class BankFactory
 {
 	public abstract SavingAcc getNewSavingAccount(int accNo,String accNm,float accBal,boolean isSalaried);
-	public  abstract	CurrentAcc getNewCurrentAccount(int accNo,String accNm,float accBal,float creditLimit);
+	public abstract CurrentAcc getNewCurrentAccount(int accNo,String accNm,float accBal,float creditLimit);
 }
 
 
