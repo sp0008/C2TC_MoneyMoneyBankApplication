c6ee36635996ba4cd2f171569069bd079ac46386
Sai Priya <91816433+sp0008@users.noreply.github.com>
2022-08-11 16:11:03 +0530

    Update MMBankFactory.java

--- Bank Application/src/com/cg/bankapplication/MMBankFactory.java
+++ Bank Application/src/com/cg/bankapplication/MMBankFactory.java
@@ -4,18 +4,17 @@ import com.cg.framework.*;
 
 public class MMBankFactory extends BankFactory 
 {
-
 	@Override
 	public SavingAcc getNewSavingAccount(int accNo, String accNm, float accBal, boolean isSalaried) 
 	{
-		SavingAcc sa=new SavingAcc(accNo,accNm,accBal,isSalaried);
+		MMSavingAcc sa=new MMSavingAcc(accNo,accNm,accBal,isSalaried);
 		return sa;
 	}
 
 	@Override
 	public CurrentAcc getNewCurrentAccount(int accNo, String accNm, float accBal, float creditLimit)
 	{
-		CurrentAcc ca=new CurrentAcc(accNo,accNm,accBal,creditLimit);
+		MMCurrentAcc ca=new MMCurrentAcc(accNo,accNm,accBal,creditLimit);
 		return ca;
 	}
 }
