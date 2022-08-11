eadbae586502158ccc0e89babffd2b27c6eb6dfd
Sai Priya <91816433+sp0008@users.noreply.github.com>
2022-08-11 16:16:18 +0530

    Update Client.java

--- Bank Application/src/com/cg/client/Client.java
+++ Bank Application/src/com/cg/client/Client.java
@@ -4,15 +4,12 @@ import com.cg.bankapplication.*;
 import com.cg.framework.*;
 
 public class Client {
-
-	public static void main(String[] args) {
-
+public static void main(String[] args) {
 		BankFactory bf=new MMBankFactory();
 		SavingAcc sa=new MMSavingAcc(234567,"ISHA",100000,true);
 		CurrentAcc ca=new MMCurrentAcc(32456,"Shiva",250000,5000);
 		sa.withdraw(sa.getAccBal());
 		ca.withdraw(ca.getCreditLimit());
-
 	}
 
 }
