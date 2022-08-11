1d533aee00e97a810ed7e45f103734e767f17c06
Sai Priya <91816433+sp0008@users.noreply.github.com>
2022-08-11 16:29:24 +0530

    Update CurrentAcc.java

--- Bank Application/src/com/cg/framework/CurrentAcc.java
+++ Bank Application/src/com/cg/framework/CurrentAcc.java
@@ -1,7 +1,6 @@
 package com.cg.framework;
 
-public class CurrentAcc extends BankAcc 
-{
+public abstract class CurrentAcc extends BankAcc {
 	private final float creditLimit=1000;
 
 	public CurrentAcc(int accNo, String accNm, float accBal,float creditLimit) 
@@ -9,17 +8,20 @@ public class CurrentAcc extends BankAcc
 		super(accNo, accNm, accBal);
 
 	}
+	
 	public void withdraw(float creditLimit)
 	{
 		System.out.println("Credit Limit available:" +creditLimit);
 	}
-	@Override
-	public String toString() {
-		return "CurrentAcc [creditLimit=" + creditLimit + "]";
-	}
+	
 	public float getCreditLimit()
 	{
 		return creditLimit;
+	}
+	
+	@Override
+	public String toString() {
+		return "CurrentAcc [creditLimit=" + creditLimit + "]";
 	}
 }
 
