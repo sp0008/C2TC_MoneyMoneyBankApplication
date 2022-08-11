e324e7becb003e5f3b9c8b27d836dea03ac19cd6
Sai Priya <91816433+sp0008@users.noreply.github.com>
2022-08-11 16:14:16 +0530

    Update MMSavingAcc.java

--- Bank Application/src/com/cg/bankapplication/MMSavingAcc.java
+++ Bank Application/src/com/cg/bankapplication/MMSavingAcc.java
@@ -4,18 +4,18 @@ import com.cg.framework.SavingAcc;
 
 public class MMSavingAcc extends SavingAcc 
 {
-	private static final float MINBAL=2000;
+	private static final float MINBAL=1500;
 
 	public MMSavingAcc(int accNo, String accNm, float accBal, boolean isSalaried) 
 	{
 		super(accNo, accNm, accBal, isSalaried);
-
 	}
 
 	public void withdraw(float accBal)
 	{
 		System.out.println("Withdrawl amount:"+accBal);
 	}
+	
 	@Override
 	public String toString()
 	{
